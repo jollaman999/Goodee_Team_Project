735a4d0858d1059a909d683c9cbc50c7ba6f0160
jollaman999 <admin@jollaman999.com>
2019-07-15 02:29:22 +0900

    Implement CategoryGroup

---
+++ petshop/src/main/java/logic/CategoryGroup.java
@@ -0,0 +1,22 @@
+package logic;
+
+public class CategoryGroup {
+    private int group_code;
+    private String group_name;
+
+    public int getGroup_code() {
+        return group_code;
+    }
+
+    public void setGroup_code(int group_code) {
+        this.group_code = group_code;
+    }
+
+    public String getGroup_name() {
+        return group_name;
+    }
+
+    public void setGroup_name(String group_name) {
+        this.group_name = group_name;
+    }
+}
