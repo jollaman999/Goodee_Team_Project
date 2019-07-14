97351726e396cc3f2a089e14a8f90728e6aa6432
jollaman999 <admin@jollaman999.com>
2019-07-15 02:31:24 +0900

    Implement CategoryItem

---
+++ petshop/src/main/java/logic/CategoryItem.java
@@ -0,0 +1,31 @@
+package logic;
+
+public class CategoryItem {
+    private int group_code;
+    private int code;
+    private String name;
+
+    public int getGroup_code() {
+        return group_code;
+    }
+
+    public void setGroup_code(int group_code) {
+        this.group_code = group_code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
