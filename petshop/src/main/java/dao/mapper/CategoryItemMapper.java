c5665d2cc71c9e95290c536d8541f7133412990f
jollaman999 <admin@jollaman999.com>
2019-07-16 21:23:40 +0900

    Fix Category related codes

--- petshop/src/main/java/dao/mapper/CategoryItemMapper.java
+++ petshop/src/main/java/dao/mapper/CategoryItemMapper.java
@@ -6,11 +6,10 @@ import org.apache.ibatis.annotations.Select;
 import logic.CategoryItem;
 
 public interface CategoryItemMapper {
-	
-	@Select("select ifnull(max(group_code),0) from category_item")
-	int maxgroup_code();
-	
-	@Insert("insert into category_item (group_code,code,name)" + 
-	                     "values (#{group_code},#{code},#{name})")
-	int insert(CategoryItem categoryitem); 
+    @Select("select ifnull(max(group_code),0) from category_item")
+    int maxitem_code();
+
+    @Insert("insert into category_item (group_code, code, name)" +
+            "values (#{group_code}, #{code}, #{name})")
+    int insert(CategoryItem categoryitem);
 }
