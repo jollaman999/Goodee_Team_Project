805a722ecced594342f65c8ed78d69337c3b6af6
dhtpgus1200 <dhtpgus1200@naver.com>
2019-07-25 15:34:41 +0900

    Board work

--- petshop/src/main/java/controller/BoardController.java
+++ petshop/src/main/java/controller/BoardController.java
@@ -78,8 +78,9 @@ public class BoardController {
     }
 
     @PostMapping("write")
-    public ModelAndView writeBoard(HttpSession session, Integer type, String member_id, MultipartHttpServletRequest request, @Valid Board board) {
+    public ModelAndView writeBoard(HttpSession session, Integer type, Integer item_no, String member_id, MultipartHttpServletRequest request, @Valid Board board) {
         board.setType(type);
+        board.setItem_no(item_no);
         board.setMember_id(member_id);
 
         ModelAndView mav = new ModelAndView("/alert");
